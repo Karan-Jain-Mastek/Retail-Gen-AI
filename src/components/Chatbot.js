4c55713e031b20ee8e033b59469384fabbedba3e
Karan-Jain-Mastek <Karan.Jain@mastek.com>
2024-11-15 01:18:02 +0530

    Correct Image Logos Check 2

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -38,21 +38,21 @@ const Chatbot = () => {
 
         const firstImage = document.querySelector('img[src="https://image.isu.pub/220606134901-e7dc18a7c73e787292ff81100bf50b56/jpg/page_1_thumb_large.jpg"] [alt="logo"] [style="width: 40px; height: 40px; margin-right: 10px; border-radius: 12.5px"]');
         if (firstImage) {
-          firstImage.src = 'mastek_branding_logo_image.jpg';
+          firstImage.src = '/mastek_branding_logo_image.jpg';
         } else {
             console.log('First image not found.');
         }
   
         const secondImage = document.querySelector('img[alt="Logo"] [src="https://image.isu.pub/220606134901-e7dc18a7c73e787292ff81100bf50b56/jpg/page_1_thumb_large.jpg"] [style="width: 30px; height: 30px; top: 21%; border-radius: 50%; margin-left: 4px"]');
         if (secondImage) {
-          secondImage.src = 'mastek_branding_logo_image.jpg';
+          secondImage.src = '/mastek_branding_logo_image.jpg';
         } else {
             console.log('Second image not found.');
         }
   
         const thirdImage = document.querySelector('img[src="https://image.isu.pub/220606134901-e7dc18a7c73e787292ff81100bf50b56/jpg/page_1_thumb_large.jpg"] [alt="Company Logo"] [style="width: 100px; height: 100px; margin-bottom: 2px; border-radius: 12.5px"]');
         if (thirdImage) {
-          thirdImage.src = 'mastek_logo.jpg';
+          thirdImage.src = '/chatbot.png';
         } else {
             console.log('Third image not found.');
         }

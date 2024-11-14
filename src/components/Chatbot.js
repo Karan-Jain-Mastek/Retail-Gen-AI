374bb6709e2b3a53f15790b155997252e453ddc3
Karan-Jain-Mastek <Karan.Jain@mastek.com>
2024-11-15 02:17:47 +0530

    Replace Image Logos Check

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -18,10 +18,6 @@ const Chatbot = () => {
         try {
           new window.WidgetV3();
           console.log('WidgetV3 initialized successfully.');
-          // Wait a little bit after initialization for images to render
-          setTimeout(() => {
-            replaceLogos(); // Try replacing logos after a short delay
-          }, 3000); 
 
           // Use MutationObserver to detect when images are added to the DOM
           observeImageChanges();
@@ -36,33 +32,6 @@ const Chatbot = () => {
     // Ensure the widget script is loaded and then initialize it
     script.onload = initializeWidget;
 
-    // Image replacement function
-    const replaceLogos = () => {
-
-        const firstImage = document.querySelector('img[src="https://image.isu.pub/220606134901-e7dc18a7c73e787292ff81100bf50b56/jpg/page_1_thumb_large.jpg"] [alt="logo"] [style="width: 40px; height: 40px; margin-right: 10px; border-radius: 12.5px"]');
-        if (firstImage) {
-          firstImage.src = '/mastek_branding_logo_image.jpg';
-        } else {
-            console.log('First image not found.');
-        }
-  
-        const secondImage = document.querySelector('img[alt="Logo"] [src="https://image.isu.pub/220606134901-e7dc18a7c73e787292ff81100bf50b56/jpg/page_1_thumb_large.jpg"] [style="width: 30px; height: 30px; top: 21%; border-radius: 50%; margin-left: 4px"]');
-        if (secondImage) {
-          secondImage.src = '/mastek_branding_logo_image.jpg';
-        } else {
-            console.log('Second image not found.');
-        }
-  
-        const thirdImage = document.querySelector('img[src="https://image.isu.pub/220606134901-e7dc18a7c73e787292ff81100bf50b56/jpg/page_1_thumb_large.jpg"] [alt="Company Logo"] [style="width: 100px; height: 100px; margin-bottom: 2px; border-radius: 12.5px"]');
-        if (thirdImage) {
-          thirdImage.src = '/chatbot.png';
-        } else {
-            console.log('Third image not found.');
-        }
-
-        console.log('All logos replaced successfully.');
-      };
-
     // Use MutationObserver to watch for DOM changes and detect image insertion
     const observeImageChanges = () => {
       const observer = new MutationObserver(() => {
@@ -75,7 +44,7 @@ const Chatbot = () => {
           // Replace the images as soon as all are found
           firstImage.src = '/mastek_branding_logo_image.jpg';
           secondImage.src = '/mastek_branding_logo_image.jpg';
-          thirdImage.src = '/mastek_logo.jpg';
+          thirdImage.src = '/chatbot.png';
 
           console.log('Logos replaced successfully.');
           observer.disconnect(); // Stop observing after images are replaced
